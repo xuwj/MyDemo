216d170a0e83e87ae7415c240b39ae118b0981e8
Yang Songxiang <yangsongxiang@caredear.com>
2015-11-04 14:44:24 +0800

    I hope this is a good start point for demo Plugin

--- src/android/MyDemo.java
+++ src/android/MyDemo.java
@@ -2,11 +2,13 @@ package org.ioniconline;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.util.Log;
 /**
  * This class echoes a string called from JavaScript.
  */
@@ -15,18 +17,14 @@ public class MyDemo extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+            Log.e("MyDemo", "process the req");
+            String str = android.os.Build.VERSION.RELEASE;
+            PluginResult r = new PluginResult(PluginResult.Status.OK, str);
+            r.setKeepCallback(true);
+            callbackContext.sendPluginResult(r);
+
             return true;
         }
         return false;
-    }
-
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
     }
 }
